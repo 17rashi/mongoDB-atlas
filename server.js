4040db68c2cd4393d820bf943b60d99cd9ab1148
17rashi <17rashishukla@gmail.com>
2024-02-23 01:00:58 +0530

    shifted mongoDB server to MONGODB Atlas- online server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const db = require("./db");
+require("dotenv").config();
 
 //now its a time to parse the data recieved
 const bodyParser = require("body-parser");
@@ -11,12 +12,16 @@ app.get("/", (req, res) => {
   res.send("server is started");
 });
 
+//import the router files
 const personRoutes = require("./routes/personRoutes");
-app.use("/person", personRoutes);
-
 const menuRoutes = require("./routes/menuRoutes");
+
+//Use the routers
+app.use("/person", personRoutes);
 app.use("/menu", menuRoutes);
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log("Port is listening to 3000");
 });
